389bb5f0d3d1c5ba008747cca448790b2231827c
chanon-mike <zzmikezz8520@gmail.com>
2023-07-13 23:55:13 +0900

    Deploy serverless

--- server/api/main.py
+++ server/api/main.py
@@ -1,9 +1,12 @@
-from fastapi import Depends, FastAPI
+import time
+
+from fastapi import Depends, FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import HTTPBearer
+from mangum import Mangum
 
-from api.core.config import Settings, get_settings
-from api.schemas.payload import Payload
+from api.config import Settings, get_settings
+from api.models.payload import Payload
 from api.security.dependencies import verify_token
 
 app = FastAPI()
@@ -50,3 +53,17 @@ def private(token: Payload = Depends(verify_token)):
 @app.get("/")
 def root():
     return {"Hello": "World"}
+
+
+handler = Mangum(app)  # for AWS Lambda
+
+
+# add middleware which calculates time of the request processing
+# and assign it to the response header
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time-Sec"] = str(process_time)
+    return response
