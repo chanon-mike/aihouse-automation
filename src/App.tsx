7d148ab19dafc002520fd3108855d02b3f250b5e
yutam29 <yuki1129taniguchi@gmail.com>
2023-07-15 22:01:01 +0900

    fix a file

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from 'react-router-dom';
 import Router from './pages/router';
-//import { Auth0ProviderWithNavigate } from './utils/auth0';
+import { Auth0ProviderWithNavigate } from './utils/auth0';
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Auth0ProviderWithNavigate>
         <Router />
+      </Auth0ProviderWithNavigate>
     </BrowserRouter>
   );
 };
